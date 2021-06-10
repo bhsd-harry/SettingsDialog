bec446435adadf4d37aed5061fda2b77c4021651
bhsd <2545473905@qq.com>
2021-06-09 21:16:41 -0400

    disabled

--- lib.js
+++ lib.js
@@ -4,6 +4,7 @@
     const isModule = (name) => ['loading', 'loaded', 'executing', 'ready'].includes( `ext.gadget.${name}` ),
         ns = mw.config.get('wgNamespaceNumber'),
         action = mw.config.get('wgAction'),
+        mySkin = mw.config.get('skin'),
         lib = [ // 修改这个数组以增删用户小工具
         {name: 'summaryFace', items: [ // 界面工具
             {key: 'highlight', type: 'Dropdown', options: [
@@ -21,7 +22,17 @@
                 if (ns < 0 || !['view', 'submit'].includes( action )) { return; }
                 mw.loader.load('//cdn.jsdelivr.net/gh/bhsd-harry/LLWiki@2.5/otherwiki/gadget-code-prettify.min.js');
                 mw.loader.load('//cdn.jsdelivr.net/gh/bhsd-harry/LLWiki@2.5/otherwiki/gadget-code-prettify.min.css', 'text/css');
-            }}], config: {disabled: isModule( 'code-prettify' )}}
+            }}], config: {disabled: isModule( 'code-prettify' )}},
+            {key: 'toBottom', type: 'Dropdown', options: [
+                {data: 'none', label: '无'},
+                {data: 'leranjun', label: 'Leranjun版', disabled: mySkin != 'vector', callback: () => {
+                mw.loader.load('https://zh.moegirl.org.cn/index.php?title=User:Leranjun/js/ScrollToBottom.js&action=raw&ctype=text/javascript');
+            }},
+                {data: 'bhsd', label: 'Bhsd版', callback: () => {
+                if (ns < 0 || !['view', 'submit'].includes( action )) { return; }
+                mw.loader.load('https://cdn.jsdelivr.net/gh/bhsd-harry/LLWiki@2.5/otherwiki/gadget-mobile-Backtotop.min.js');
+                mw.loader.load('https://cdn.jsdelivr.net/gh/bhsd-harry/LLWiki@2.5/otherwiki/gadget-mobile-Backtotop.min.css', 'text/css');
+            }}]}
         ]},
         {name: 'summaryEdit', items: [ // 编辑工具
         ]},
@@ -31,14 +42,13 @@
         ]},
         {name: 'summaryTest', items: [ // 测试性工具
         ]}
-    ],
-        mySkin = mw.config.get('skin');
+    ];
     mw.gadgets.json.then(() => {
         lib.forEach(({name, items}) => {
             const settings = mw.gadgets[name];
-            items.forEach(({key, skin, options}) => {
-                if (skin && skin != mySkin) { return; }
-                options.filter(({data}) => settings[key].includes( data ))
+            items.forEach(({key, skin, options, config = {}}) => {
+                if (skin && skin != mySkin || config.disabled) { return; }
+                options.filter(({data, disabled}) => settings[key].includes( data ) && !disabled)
                     .forEach(({callback = () => {}}) => { callback(); });
             });
         });
